e5501d0aabb11ef9df44f492016fdd2cddda9d50
Christopher Rossi <61894684+DuckyZero@users.noreply.github.com>
2021-11-10 12:03:10 -0500

    Add files via upload
    Created main file to be used for Keybytes application

---
+++ keybytes.py
@@ -0,0 +1,10 @@
+# Names: Christopher Rossi, Paige Broussard, and Josh Degazio
+# Project: Keybytes - voice activated soundboard
+# Description: Keybytes is a school group project for CP317 (Software Enginnering) 
+#              and it is a voice activated soundboard to help handicapped individuals
+#              play their favorite sound clips to their friends!
+# Start Date: 11-05-2021
+# End Date: ??/l
+
+class soundBoard():
+    def
