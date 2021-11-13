7c64d01d619255dc3b0de6ff8be60f3e24a6fa8f
Josh D <31970872+ResurrectHD@users.noreply.github.com>
2021-11-13 09:26:06 -0500

    Update keybytes.py

--- keybytes.py
+++ keybytes.py
@@ -5,6 +5,26 @@
 #              play their favorite sound clips to their friends!
 # Start Date: 11-05-2021
 # End Date: ??/l
+# Keybyte = the composite of keybind andsoundbyte
+# Keybind = button(s) pressed in succession that do something (in this case play a soundbyte)
+# Soundbyte = short audio clip that is usually funny, reactionary, or relatable.
 
 class soundBoard():
-    def
+    #Need a keybind global string array that gets loaded into the program on startup from a file.
+
+
+    def create_keybyte():
+        tempKeybind = input("Type the keys, seperated by a plus, that you would like to assign a soundbyte to: ")
+        tempSoundbyte = None
+
+        while tempSoundbyte.endswith(".mp3") == False:
+            tempSoundbyte = input("Now enter the address of the mp3 file you wish to use as a soundbyte: ")
+            if tempSoundbyte.endswith(".mp3") == False:
+                print("You have entered an incorrect file address to an mp3 file. Please try again.")
+        #save_keybyte(tempSoundbyte, tempKeybind)
+        
+    def save_keybyte(tempSoundbyte, tempKeybind):
+        #Save keybyte to file so it can be loaded on startup
+        #and potentially save the soundbyte itself to a folder for easier access.
+
+    #Need to somehow constantly listen to the keyboard so that no matter what the user is doing, they can use their keybytes.
