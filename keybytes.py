065b204c94e0d9ca5e70dc6cdf691d68a3cdd6a8
Josh D <31970872+ResurrectHD@users.noreply.github.com>
2021-11-24 17:03:32 -0500

    Update keybytes.py

--- keybytes.py
+++ keybytes.py
@@ -8,23 +8,40 @@
 # Keybyte = the composite of keybind andsoundbyte
 # Keybind = button(s) pressed in succession that do something (in this case play a soundbyte)
 # Soundbyte = short audio clip that is usually funny, reactionary, or relatable.
+import os
 
 class SoundBoard():
-    # Need a keybind global string array that gets loaded into the program on startup from a file.
+    #TODO Need to update this dictionary based on keybinds text file on startup.
+    keybytes = {}
+
+    def __init__(self, dpath):
+        self.dpath = dpath
+    
+    #Creates folder in Appdata/Roaming, if one does not already exist, to store soundbytes.
+    def dirCheck(self):
+        #If it doesn't exist, create folder
+        if not os.path.exists(self.dpath):
+            os.makedirs(self.dpath)
+            os.makedirs('%sSounds\\' % self.dpath)
+            f = open('%skeybinds.txt' % self.dpath, 'x')
+            f.close
+            #Create all other files we need here.
 
     def create_keybyte(self):
-        temp_keybind = input("Type the keys, separated by a plus, that you would like to assign a soundbyte to: ")
+        temp_keybind = input('Type the keys, separated by a plus, that you would like to assign a soundbyte to: ')
         temp_soundbyte = None
 
-        while not temp_soundbyte.endswith(".mp3"):
-            temp_soundbyte = input("Now enter the address of the mp3 file you wish to use as a soundbyte: ")
-            if not temp_soundbyte.endswith(".mp3"):
-                print("You have entered an incorrect file address to an mp3 file. Please try again.")
-        # save_keybyte(tempSoundbyte, tempKeybind)
+        while not temp_soundbyte.endswith('.mp3'):
+            temp_soundbyte = input('Now enter the address of the mp3 file you wish to use as a soundbyte: ')
+            if not temp_soundbyte.endswith('.mp3'):
+                print('You have entered an incorrect file address for an mp3 file. Please try again.')
 
-    def save_keybyte(self, temp_soundbyte, temp_keybind):
-        pass
-# Save keybyte to file so it can be loaded on startup
-# and potentially save the soundbyte itself to a folder for easier access.
+        self.keybytes[temp_keybind] = temp_soundbyte
+        #TODO Add keybyte to keybinds text file
 
-# Need to somehow constantly listen to the keyboard so that no matter what the user is doing, they can use their keybytes.
+
+#TODO Need to somehow constantly listen to the keyboard so that no matter what the user is doing, they can use their keybytes.
+
+#Create soundboard instance and ensure that if the folder doesnt already exist it is created
+sBoard = SoundBoard('%s\\KeyBytes\\' %  os.environ['APPDATA'] )
+sBoard.dirCheck()
